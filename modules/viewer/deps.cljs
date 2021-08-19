6cb7ce13bd1447a6c599e4b2402e54e270094e77
Martin Kavalar <mk@katercalling.com>
2021-08-19 20:30:45 +0200

    Move deps to markdown module, regen package.json, bb.edn touch-up

--- modules/viewer/deps.cljs
+++
@@ -1,5 +0,0 @@
-{:npm-deps {"js-yaml"                "^4.1.0"
-            "markdown-it-task-lists" "^2.1.1"
-            "markdown-it-texmath"    "^0.9.1"
-            "punycode"               "^2.1.1"
-            "process"                "^0.11.10"}}
