df5636ccf2157cbff02d5ce950ae718634d15240
Andrea Amantini <lo.zampino@gmail.com>
2021-09-15 13:19:48 +0200

    Support Images both inline and and as blocks

--- modules/markdown/src/nextjournal/markdown.cljs
+++ modules/markdown/src/nextjournal/markdown.cljs
@@ -4,6 +4,7 @@
             ["markdown-it/lib/token" :as Token]
             ["markdown-it-task-lists" :as md-task-lists]
             ["markdown-it-texmath" :as md-texmath]
+            ["markdown-it-block-image" :as md-block-image]
             ["katex" :as katex]
             [nextjournal.markdown.github-preamble :as github-preamble]
             [nextjournal.markdown.todo-lists :as todo-lists]
@@ -19,16 +20,18 @@
    (doto ^js (md #js{:breaks false :html true :linkify true})
      (.use md-task-lists #js{:label true})
      (.use md-texmath #js{:delimiters "dollars" :engine katex})  ;; $$ or $
-
      #_#_ ;; TODO: support these
      (use md-texmath #js{:delimiters "brackets"}) ;; \[ \] or \( \)
      (use md-texmath #js{:delimiters "latex"}) ;; \begin{env} \end{env} (block only)
-
      ;; add tailwind styling to lists
      (.use md-class #js{:ul "list-disc list-inside"
                         :ol "list-decimal list-inside"})
+     ;; TODO: convert to js in order to be used from clj
      (.use github-preamble/Plugin)
-     (.use todo-lists/Plugin))))
+     (.use todo-lists/Plugin)
+     ;; detects images in block position (doesn't wrap into a paragraph)
+     ;; NOTE: in nextjournal it would be extremely useful to do the same for links e.g. [text](path)
+     (.use md-block-image, #js {:outputContainer nil}))))
 
 (defn parse [text] (.parse @Markdown text {}))
 
