6e7a65b414d78e9204809cb4d21d699e46638d01
Andrea Amantini <lo.zampino@gmail.com>
2021-09-15 19:14:49 +0200

    Add TOC to devcard

--- modules/viewer/src/nextjournal/viewer.cljs
+++ modules/viewer/src/nextjournal/viewer.cljs
@@ -697,6 +697,8 @@
    (view-as :hiccup
             (-> "# Markdown to Hiccup
 
+[[TOC]]
+
 and _some_ par for fun break\nhere softly
 
 one par below
@@ -705,13 +707,19 @@ one par below
 -  bullet **two**
 -  bullet foo
 
+## Images
+
 images inline ![alt](https://nextjournal.com/images/nextjournal-logo.svg) or as block:
 
 ![Some **nice** caption](https://nextjournal.com/images/nextjournal-logo.svg \"A nice logo\")
 
+
+## Rulers
 ---
 
 > with a quote
+
+### Code
 
 ```clj
 (reduce + 0 (range 0 10))
