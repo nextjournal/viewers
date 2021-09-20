eb64c40e1868a17da90c11ed7db7e1f1f76265cd
Andrea Amantini <lo.zampino@gmail.com>
2021-09-20 10:09:20 +0200

    Remove markdownit plugins related to rendering
    since rendering is handled via hiccup transformation of markdown data, 
    we'll rebuild markup structure in a later step.

--- modules/markdown/src/deps.cljs
+++ modules/markdown/src/deps.cljs
@@ -1,6 +1,4 @@
-{:npm-deps {"@toycode/markdown-it-class"      "1.2.4"
-            "js-yaml"                         "^4.1.0"
+{:npm-deps {"js-yaml"                         "^4.1.0"
             "markdown-it"                     "^10.0.0"
-            "markdown-it-task-lists"          "^2.1.1"
             "markdown-it-texmath"             "^0.9.1"
             "punycode"                        "^2.1.1"}}
--- modules/markdown/src/nextjournal/markdown.cljs
+++ modules/markdown/src/nextjournal/markdown.cljs
@@ -1,8 +1,6 @@
 (ns nextjournal.markdown
-  (:require ["@toycode/markdown-it-class" :as md-class]
-            ["markdown-it" :as md]
+  (:require ["markdown-it" :as md]
             ["markdown-it/lib/token" :as Token]
-            ["markdown-it-task-lists" :as md-task-lists]
             ["markdown-it-texmath" :as md-texmath]
             ["markdown-it-block-image" :as md-block-image]
             ["markdown-it-toc-done-right" :as md-toc]
@@ -19,21 +17,18 @@
 (def Markdown
   (delay
    (doto ^js (md #js{:breaks false :html true :linkify true})
-     (.use md-task-lists #js{:label true})
      (.use md-texmath #js{:delimiters "dollars" :engine katex})  ;; $$ or $
      #_#_ ;; TODO: support these
      (use md-texmath #js{:delimiters "brackets"}) ;; \[ \] or \( \)
      (use md-texmath #js{:delimiters "latex"}) ;; \begin{env} \end{env} (block only)
      ;; add tailwind styling to lists
-     (.use md-class #js{:ul "list-disc list-inside"
-                        :ol "list-decimal list-inside"})
      ;; TODO: convert to js in order to be used from clj
      (.use github-preamble/Plugin)
      (.use todo-lists/Plugin)
      ;; detects images in block position (doesn't wrap into a paragraph)
      ;; NOTE: in nextjournal it would be extremely useful to do the same for links e.g. [text](path)
      (.use md-block-image)
-     ;; [[TOC]]
+     ;; [[TOC]] ; TODO: write our own / we need the parsing pass only
      (.use md-toc)
      )))
 
