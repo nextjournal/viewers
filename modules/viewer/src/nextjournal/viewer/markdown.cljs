57ae54ead3a8e638b1577cdd480b399eccaad782
Philippa Markovics <philippa@markovics.com>
2021-09-27 12:36:17 +0200

    Better markdown reference card, fix styles

--- modules/viewer/src/nextjournal/viewer/markdown.cljs
+++ modules/viewer/src/nextjournal/viewer/markdown.cljs
@@ -3,5 +3,5 @@
 
 (defn viewer [value]
   (when value
-    {:nextjournal/value  (md/->hiccup value)
+    {:nextjournal/value [:div.viewer-markdown (md/->hiccup value)]
      :nextjournal/viewer :hiccup}))
