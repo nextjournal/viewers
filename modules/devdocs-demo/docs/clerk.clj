723a59428528f6e9b519b773b268c15452be8c94
Arne Brasseur <arne@arnebrasseur.net>
2021-09-29 11:10:59 +0200

    Devdocs: fix routes, configurable logo, examples
    - Change the signature of the views to match how they're being called
    - Make the logo configurable via a goog-define
    - Make sure there's an example of each type of notebook

---
+++ modules/devdocs-demo/docs/clerk.clj
@@ -0,0 +1,11 @@
+(ns notebooks.clerk)
+
+;; # A Clerk Notebook
+
+;; This is a Clerk notebook, it is written as Clojure plus comments like this
+;; one.
+;;
+;; Comments can contain markdown. When rendering this in devdocs the code is
+;; evaluated once, and the results are cached.
+
+(seq (.split (System/getProperty "java.class.path") ":"))
