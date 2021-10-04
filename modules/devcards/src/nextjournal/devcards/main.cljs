235fbf02688cd16baacb15226c015097b9af39a8
Martin Kavalar <mk@katercalling.com>
2021-10-04 21:41:46 +0200

    Fix compat with newer reagent by using render from reagent.dom

--- modules/devcards/src/nextjournal/devcards/main.cljs
+++ modules/devcards/src/nextjournal/devcards/main.cljs
@@ -1,8 +1,8 @@
 (ns nextjournal.devcards.main
   (:require [nextjournal.devcards.routes :as router]
-            [reagent.core :as r]
+            [reagent.dom :as rdom]
             [reitit.frontend.easy :as rfe]))
 
 (defn ^:export ^:dev/after-load init []
   (rfe/start! router/router #(reset! router/match %1) {:use-fragment @router/use-fragment?})
-  (r/render [router/devcards] (js/document.getElementById "app")))
+  (rdom/render [router/devcards] (js/document.getElementById "app")))
