66872d8c75c4a2b310fb382454a05156c632d518
Andrea Amantini <lo.zampino@gmail.com>
2021-10-05 18:41:14 +0200

    Define a standard set of render overrides for the markdown viewer
    in order to have same or better output than with markdown-it render 
    function.
    
    Also move cards under the markdown namespace.

--- modules/viewer/src/nextjournal/viewer.cljs
+++ modules/viewer/src/nextjournal/viewer.cljs
@@ -649,10 +649,6 @@
 (dc/defcard viewer-markdown
   [inspect (view-as :markdown "# Supported Markdown
 
-## Table of Contents
-
-[[toc]]
-
 ## Paragraphs
 
 To create paragraphs, use a blank line to separate one or more lines of text.
@@ -792,76 +788,3 @@ as building hiccup is recursive, we're using the specific viewers for values occ
                                                             (max 0)
                                                             (min 100)
                                                             (str "%"))}}]]])))]])
-
-(dc/defcard markdown-dark-mode
-  "Provide custom styles to e.g. support dark mode."
-  [markdown]
-  [inspect
-   (view-as
-     :hiccup
-     (->> @markdown
-          (md/->hiccup
-           (assoc md.data/->hiccup-ctx ;; customizes the main container
-                  :doc (partial md.data/into-markup [:div.viewer-markdown.dark:bg-gray-900.dark:text-white.rounded.shadow-sm.p-4])))))]
-  {::dc/state "### Dark Mode Support
-Here is some code that provides a custom wrapper with styles to e.g. set the text color
-and background if dark mode is enabled in your system."})
-
-(defn show-formula [_ctx node]
-  [inspect (view-as :latex (md.data/->text node))])
-
-(defn sidenote-click-handler [^js e]
-  (when (.. e -target -classList (contains "sidenote-ref"))
-    (.. e -target -classList (toggle "expanded"))))
-
-(dc/defcard markdown-plugins
-  [markdown]
-  [inspect (view-as :hiccup
-                    (->> @markdown
-                         (md/->hiccup (assoc md.data/->hiccup-ctx
-                                             :doc (partial md.data/into-markup [:div.viewer-markdown {:on-click sidenote-click-handler}])
-                                             :formula show-formula
-                                             :todo-list (partial md.data/into-markup [:ul.contains-task-list])
-                                             :todo-item (fn [ctx {:as node :keys [attrs]}]
-                                                          (md.data/into-markup [:li [:input {:type "checkbox" :checked (:checked attrs)}]]
-                                                                               ctx
-                                                                               node))))))]
-  {::dc/state "# Markdown Default Plugins
-## Sidenotes
-
-One of the most distinctive features of Tufte’s style is his extensive use of sidenotes.
-Sidenotes[^1] are like footnotes, except they don’t force the reader to jump their eye to
-the bottom of the page, but instead display off to the side in the margin.[^longnote]
-
-[^1]: Here’s a sidenote.
-
-[^longnote]: And here is another one with more text that wraps over multiple lines.
-
-Sidenotes are a great example of the web not being like print. On sufficiently large viewports,
-Tufte CSS uses the margin for sidenotes, margin notes, and small figures. On smaller viewports,
-elements that would go in the margin are hidden until the user toggles them into view.
-
-The goal is to present related but not necessary information such as asides or citations as
-close as possible to the text that references them. At the same time, this secondary information
-should stay out of the way of the eye, not interfering with the progression of ideas in the
-main text.
-
-## Todos
-
-- [x] Checked
-- [ ] Unchecked
-  - [ ] Nested unchecked
-
-## Formulas
-
-### Block Formulas
-
-$$\\int_{\\omega} \\phi d\\omega$$
-
-### Formulas inside tables
-
-| Syntax |  JVM                     | JavaScript                                    |
-|--------|-------------------------:|:----------------------------------------------|
-| foo    |  Local_Date_             | goog.date.Date                                |
-| bar    |  java.time.LocalDateTime | $\\bigoplus_{\\alpha < \\omega}\\phi_\\alpha$ |
-"})
--- modules/viewer/src/nextjournal/viewer/markdown.cljs
+++ modules/viewer/src/nextjournal/viewer/markdown.cljs
@@ -1,7 +1,118 @@
 (ns nextjournal.viewer.markdown
-  (:require [nextjournal.markdown :as md]))
+  (:require [nextjournal.markdown :as md]
+            [nextjournal.markdown.data :as md.data]
+            [nextjournal.devcards :as dc]))
+
+;; FIXME: inspect should be passed down to viewers via ctx or something
+(defn inspect* [& args]
+  (let [i (resolve 'nextjournal.viewer/inspect)]
+    (apply i args)))
+
+(defn sidenote-click-handler [^js e]
+  (when (.. e -target -classList (contains "sidenote-ref"))
+    (.. e -target -classList (toggle "expanded"))))
+
+(def default-viewers
+  (assoc md.data/default-renderers
+         :doc     (partial md.data/into-markup [:div.viewer-markdown {:on-click sidenote-click-handler}])
+         :code    (fn [_ctx node] [:div.viewer-code
+                                   [inspect* {:nextjournal/viewer :code
+                                              :nextjournal/value (md.data/->text node)}]])
+         :formula (fn [_ctx node] [inspect* {:nextjournal/viewer :latex
+                                             :nextjournal/value (md.data/->text node)}])))
 
 (defn viewer [value]
+  ;; TODO: allow to pass "modes" with different sets of overrides
   (when value
-    {:nextjournal/value [:div.viewer-markdown (md/->hiccup value)]
+    {:nextjournal/value (md/->hiccup default-viewers value)
      :nextjournal/viewer :hiccup}))
+
+(dc/defcard default-markdown
+  [markdown]
+  [inspect* (viewer "# Markdown Default Rendering
+## Sidenotes
+
+One of the most distinctive features of Tufte’s style is his extensive use of sidenotes.
+Sidenotes[^1] are like footnotes, except they don’t force the reader to jump their eye to
+the bottom of the page, but instead display off to the side in the margin.[^longnote]
+
+[^1]: Here’s a sidenote.
+
+[^longnote]: And here is another one with more text that wraps over multiple lines.
+
+Sidenotes are a great example of the web not being like print. On sufficiently large viewports,
+Tufte CSS uses the margin for sidenotes, margin notes, and small figures. On smaller viewports,
+elements that would go in the margin are hidden until the user toggles them into view.
+
+The goal is to present related but not necessary information such as asides or citations as
+close as possible to the text that references them. At the same time, this secondary information
+should stay out of the way of the eye, not interfering with the progression of ideas in the
+main text.
+
+## Todos
+
+- [x] Checked
+- [x] Unchecked
+  - [ ] Nested unchecked
+
+## Code
+
+```clj
+(like what)
+```
+
+### Block Formulas
+
+$$\\int_{\\omega} \\phi d\\omega$$
+
+### Formulas inside tables
+
+| Syntax |  JVM                     | JavaScript                                    |
+|--------|-------------------------:|:----------------------------------------------|
+| foo    |  Local_Date_             | goog.date.Date                                |
+| bar    |  java.time.LocalDateTime | $\\bigoplus_{\\alpha < \\omega}\\phi_\\alpha$ |
+")])
+
+(dc/defcard dark-mode
+  "Provide custom styles to e.g. support dark mode."
+  [markdown]
+  [inspect* {:nextjournal/viewer :hiccup
+             :nextjournal/value (->> @markdown
+                                     (md/->hiccup
+                                      (assoc md.data/default-renderers
+                                             :doc (partial md.data/into-markup [:div.viewer-markdown.dark:bg-gray-900.dark:text-white.rounded.shadow-sm.p-4]))))}]
+  {::dc/state "### Dark Mode Support
+Here is some code that provides a custom wrapper with styles to e.g. set the text color
+and background if dark mode is enabled in your system."})
+
+(dc/defcard table-of-contents
+  [markdown]
+  [inspect* (viewer @markdown) ]
+  {::dc/state "# Built-in Table of Contents
+
+[[TOC]]
+
+## Section 1
+
+Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+
+### Section 1.1
+
+Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+
+## Section 2
+
+Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+
+### Section 2.1
+
+Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+
+### Section 2.2
+
+Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+
+## Section 3
+
+Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+"})
