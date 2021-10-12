c047660eba7cf3c4b6a23958c5408ac6591093a5
Arne Brasseur <arne@arnebrasseur.net>
2021-10-12 12:03:44 +0200

    Get devdocs example running again, move to /examples/devdocs
    Version bumps and API tweaks.

--- repl_sessions/dependencies.clj
+++ repl_sessions/dependencies.clj
@@ -4,12 +4,13 @@
 
 (licp/git-pull-lib "modules/command-bar/deps.edn" 're-frame/re-frame)
 (licp/git-pull-lib "modules/devcards/deps.edn" 're-frame/re-frame)
-(licp/git-pull-lib "modules/devdocs/deps.edn" 'com.lambdaisland/deja-fu)
-(licp/git-pull-lib "modules/devdocs/deps.edn" 'com.lambdaisland/shellutils)
+(licp/git-pull-lib "modules/devdocs/deps.edn" 'com.nextjournal/clerk)
+(licp/git-pull-lib "modules/devdocs/deps.edn" 're-frame/re-frame)
 (licp/git-pull-lib 'com.nextjournal/clerk)
 
-(licp/classpath-chain)
+(comment
+  (licp/classpath-chain)
 
-(licp/update-classpath! {})
+  (licp/update-classpath! {})
 
-(licp-watch/start! {})
+  (licp-watch/start! {}))
