6d0985185f54f547f31b521d2ea78a03da3e61b2
Andrea Amantini <lo.zampino@gmail.com>
2021-10-15 16:45:20 +0200

    Improve home view

--- examples/examples.cljs
+++ examples/examples.cljs
@@ -1,12 +1,12 @@
-(ns examples.main
+(ns examples
   (:require [clojure.string :as str]
             [nextjournal.devcards.routes :as devcards.routes]
             [nextjournal.devdocs.demo :as devdocs.demo]
+            [nextjournal.ui.components.icon :as icon]
             [reagent.dom :as rdom]
+            [reagent.core :as reagent]
             [reitit.core :as r]
             [reitit.frontend :as rf]
-            [reitit.frontend.history :as rfh]
-            [reagent.core :as reagent]
             [reitit.frontend.easy :as rfe]))
 
 (defonce match (reagent/atom nil))
@@ -14,19 +14,27 @@
 (def routes
   [["/"           {:name ::home}]
    ["/devdocs/*"  {:name ::devdocs  :router devdocs.demo/router    :view devdocs.demo/view}]
-   ["/devcards/*" {:name ::devcards :router devcards.routes/router :view devcards.routes/view}]
-   ])
+   ["/devcards/*" {:name ::devcards :router devcards.routes/router :view devcards.routes/view}]])
 
-(defn main []
+(defn view []
   (let [{:keys [data]} @match
         {:keys [submatch view]} data]
     (if (and submatch view)
       [view submatch]
-      [:<>
-       [:h1.flex "👋Nextjournal Viewers Examples"]
-       [:ul
-        [:a {:href "#/devdocs/docs/"} "Devdocs"]
-        [:a {:href "#/devcards/"} "Devcards"]]])))
+      ;; else home:
+      [:div.bg-gray-200.w-screen.min-h-screen.flex.items-center.justify-center.relative.sans-serif
+       [:div
+        [:div.px-8.py-6
+         [:img.inline-block {:src "https://cdn.nextjournal.com/images/nextjournal-logo.svg" :width 260}]]
+        [:div.bg-white.shadow-lg.rounded-lg.overflow-hidden.border.border-gray-300.border-b-0
+         [:ul.text-2xl
+          (for [[route title] [[:devcards/root "Devcards"]
+                               [:devdocs/index "Devdocs"]]]
+            [:li
+             [:a.text-gray-800.hover:text-indigo-600.block.px-8.justify-between.py-4.text-xl.border-b.border-gray-300.flex.items-center.bg-white-50.hover:bg-indigo-100.transition-all.ease-in-out.duration-150
+              {:href (rfe/href route)}
+              title
+              [:span.ml-2.opacity-50 "▶"]]])]]]])))
 
 (def router
   "nested router composing examples"
@@ -47,4 +55,4 @@
 
 (defn ^:export ^:dev/after-load init []
   (rfe/start! router #(reset! match %1) {:use-fragment true})
-  (rdom/render [main] (js/document.getElementById "app")))
+  (rdom/render [view] (js/document.getElementById "app")))
