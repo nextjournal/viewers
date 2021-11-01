c91ee91f9b2f5536d8f01749468106f38465d7e3
Kleopatra Pirpinia <kleopatra.pirpinia@gmail.com>
2021-11-01 15:32:25 +0100

    Replace indigo colors with gray for devcards/devdocs page

--- examples/examples.cljs
+++ examples/examples.cljs
@@ -48,13 +48,13 @@
         [:div
          [:div.px-8.py-6
           [:img.inline-block {:src "https://cdn.nextjournal.com/images/nextjournal-logo.svg" :width 260}]]
-         [:div.bg-white.shadow-lg.rounded-lg.overflow-hidden.border.border-gray-300.border-b-0
+         [:div.bg-white.shadow-md.rounded-md.overflow-hidden.border.border-gray-300.border-b-0
           [:ul.text-2xl
            (for [[route title] [[:devcards/root "Devcards"]
                                 [:devdocs/index "Devdocs"]]]
              ^{:key title}
              [:li
-              [:a.text-gray-800.hover:text-indigo-600.block.px-8.justify-between.py-4.text-xl.border-b.border-gray-300.flex.items-center.bg-white-50.hover:bg-indigo-100.transition-all.ease-in-out.duration-150
+              [:a.text-gray-800.block.px-8.justify-between.py-4.text-xl.border-b.border-gray-300.flex.items-center.bg-white-50.hover:bg-gray-100.transition-all.ease-in-out.duration-150
                {:href (rfe/href route)}
                title
                [:span.ml-2.opacity-50 "▶"]]])]]]])])) ;; FIXME: using ui.components.icon not working
