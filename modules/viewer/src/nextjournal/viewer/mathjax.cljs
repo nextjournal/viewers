55683c37aff493c39b0a45e5c715c0093239eb23
Andrea Amantini <lo.zampino@gmail.com>
2021-12-07 18:45:09 +0100

    pass 'display' options to mathjax viewer

--- modules/viewer/src/nextjournal/viewer/mathjax.cljs
+++ modules/viewer/src/nextjournal/viewer/mathjax.cljs
@@ -15,19 +15,21 @@
   {:html "https://run.nextjournalusercontent.com/data/QmaH3kKYJoHuVwUx7MYmxRFdFdvEyeJcGYFWChB3UUeDT3?filename=es5/tex-chtml-full.js&content-type=application/javascript"
    :svg "https://run.nextjournalusercontent.com/data/QmQadTUYtF4JjbwhUFzQy9BQiK52ace3KqVHreUqL7ohoZ?filename=es5/tex-svg-full.js&content-type=application/javascript"})
 
-(defn viewer* [value]
+(defn viewer* [value {:as _opts :keys [inline?] :or {inline? false}}]
   (r/with-let [!el (r/atom nil)
                !mathjax (r/atom nil)]
     (-> (d3/require (bundle-url output-format))
-        (j/call :then #(reset! !mathjax (.-MathJax js/window))))
+        (j/call :then #(do (js/console.log :MJP %) (reset! !mathjax (.-MathJax js/window)))))
     (when (and @!el @!mathjax value)
       (let [r (case output-format
-                :svg (.tex2svg ^js @!mathjax value)
+                :svg (.tex2svg ^js @!mathjax value #js {:display (not inline?)})
                 :html (.tex2chtml ^js @!mathjax value))]
         (if-let [c (.-firstChild @!el)]
           (.replaceChild @!el r c)
           (.appendChild @!el r))))
-    [:div {:ref (fn [el] (when el (reset! !el el)))}]))
+    [:span {:ref (fn [el] (when el (reset! !el el)))}]))
 
-(defn viewer [value]
-  ^{:nextjournal/viewer :reagent} [viewer* value])
+(defn viewer
+  ([value] (viewer value {}))
+  ([value opts]
+   ^{:nextjournal/viewer :reagent} [viewer* value opts]))
