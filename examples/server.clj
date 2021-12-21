0d6f26d90a7a9d4504624054c32ce5d9263b920d
Martin Kavalar <mk@katercalling.com>
2021-12-21 17:26:02 +0100

    Add handler to render Clerk notebooks on demand

---
+++ examples/server.clj
@@ -0,0 +1,23 @@
+(ns server
+  (:require [nextjournal.clerk :as clerk]
+            [nextjournal.clerk.webserver :as clerk-webserver]
+            [nextjournal.clerk.view :as clerk-view]))
+
+
+(alter-var-root #'clerk-view/resource->static-url assoc "/js/viewer.js" "http://localhost:7779/js/viewer.js")
+
+
+(defn doc->html [path]
+  (let [paths [path]
+        path->doc (into {} (map (juxt identity clerk/file->viewer)) paths)
+        path->url (into {} (map (juxt identity #(clerk/strip-index %))) paths)
+        static-app-opts {:path->doc path->doc :paths (vec (keys path->doc)) :path->url path->url :path path}]
+    (clerk-view/->static-app static-app-opts)))
+
+#_(doc->html "resources/docs/clerk.clj")
+
+(defn handler [{:as req :keys [uri]}]
+  (let [relative-path (subs uri 1)]
+    (binding [*ns* (find-ns 'user)]
+      {:body (doc->html relative-path)
+       :status 200})))
