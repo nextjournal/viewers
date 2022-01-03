84534662f8245fa3320301f1d9767df6bc9965d5
Philippa Markovics <philippa@markovics.com>
2022-01-03 16:53:52 +0100

    Start working on using tw for viewers.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,13 @@ module.exports = {
   darkMode: "media",
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ["Fira Sans", "-apple-system", "BlinkMacSystemFont", "sans-serif"],
+      serif: ["PTSerif", "serif"],
+      mono: ["Fira Mono", "monospace"],
+      display: ["Fira Sans Condensed", "-apple-system", "BlinkMacSystemFont", "sans-serif"],
+      inter: ["Inter", "-apple-system", "BlinkMacSystemFont", "sans-serif"]
+    }
   },
   variants: {
     extend: {},
