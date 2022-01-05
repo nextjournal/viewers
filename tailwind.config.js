955b5051189e25b5dbffd960bd11ef51d660f58c
Philippa Markovics <philippa@markovics.com>
2022-01-05 17:44:29 +0100

    Simplify stylesheets using tailwind 3 (#36)
    * Move tailwind base styles into viewer css and replace app.css
    * Upgrade tailwind and typography plugin
    * Use tw prose for markdown and notebook viewers, move remainder of viewers to tw

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
 module.exports = {
-  mode: "jit",
-  purge: ["./public/**/*.js"],
+  content: ["./public/**/*.js"],
   darkMode: "media",
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ["Fira Sans", "-apple-system", "BlinkMacSystemFont", "sans-serif"],
+      serif: ["PTSerif", "serif"],
+      mono: ["Fira Mono", "monospace"]
+    }
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/typography")
+  ],
 };
