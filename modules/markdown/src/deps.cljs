ce03496ec24ec27c711627371b6babb0192a5700
Philippa Markovics <philippa@markovics.com>
2022-03-02 11:57:40 +0100

    Add emoji-regex to npm deps

--- modules/markdown/src/deps.cljs
+++ modules/markdown/src/deps.cljs
@@ -1,4 +1,5 @@
-{:npm-deps {"markdown-it"                "12.2.0"
+{:npm-deps {"emoji-regex"                "^10.0.0"
+            "markdown-it"                "12.2.0"
             "markdown-it-block-image"    "0.0.3"
             "markdown-it-sidenote"       "git+https://github.com/gerwitz/markdown-it-sidenote.git"
             "markdown-it-texmath"        "0.9.1"
