f4a32b05ff4382a85a3dcf66cdec89c2389ac3c8
Andrea Amantini <lo.zampino@gmail.com>
2022-07-06 17:40:39 +0200

    Upgrade clojure mode to latest Codemirror / Lezer (#72)
    * Uses an updated clojure-mode
    * Updates CodeMirror bundle to v6.0.0
    * Updates Lezer parser bundle to v1.0.0
    * Tries to enforce `deps.cljs` convention in all modules

--- modules/viewer/src/deps.cljs
+++ modules/viewer/src/deps.cljs
@@ -1,1 +1,7 @@
-{:npm-deps {"katex" "^0.12.0"}}
+{:npm-deps
+ {"@codemirror/language" "^6.1.0"
+  "@codemirror/state" "^6.0.1"
+  "@codemirror/view" "^6.0.2"
+  "@lezer/highlight" "^1.0.0"
+  "d3-require" "^1.2.4"
+  "katex" "^0.12.0"}}
--- modules/viewer/src/nextjournal/viewer/code.cljs
+++ modules/viewer/src/nextjournal/viewer/code.cljs
@@ -1,7 +1,8 @@
 (ns nextjournal.viewer.code
-  (:require ["@codemirror/highlight" :as highlight]
+  (:require ["@codemirror/language" :refer [syntaxHighlighting HighlightStyle]]
             ["@codemirror/state" :refer [EditorState]]
             ["@codemirror/view" :refer [EditorView]]
+            ["@lezer/highlight" :refer [tags]]
             [nextjournal.clojure-mode :as cm-clj]
             [applied-science.js-interop :as j]))
 
@@ -27,8 +28,40 @@
                   ".cm-tooltip > ul > li:first-child" {:border-top-left-radius "3px"
                                                        :border-top-right-radius "3px"}})))
 
+(def highlight-style
+  (.define HighlightStyle
+    (clj->js [{:tag (.-meta tags) :class "cmt-meta"}
+              {:tag (.-link tags) :class "cmt-link"}
+              {:tag (.-heading tags) :class "cmt-heading"}
+              {:tag (.-emphasis tags) :class "cmt-italic"}
+              {:tag (.-strong tags) :class "cmt-strong"}
+              {:tag (.-strikethrough tags) :class "cmt-strikethrough"}
+              {:tag (.-keyword tags) :class "cmt-keyword"}
+              {:tag (.-atom tags) :class "cmt-atom"}
+              {:tag (.-bool tags) :class "cmt-bool"}
+              {:tag (.-url tags) :class "cmt-url"}
+              {:tag (.-contentSeparator tags) :class "cmt-contentSeparator"}
+              {:tag (.-labelName tags) :class "cmt-labelName"}
+              {:tag (.-literal tags) :class "cmt-literal"}
+              {:tag (.-inserted tags) :class "cmt-inserted"}
+              {:tag (.-string tags) :class "cmt-string"}
+              {:tag (.-deleted tags) :class "cmt-deleted"}
+              {:tag (.-regexp tags) :class "cmt-regexp"}
+              {:tag (.-escape tags) :class "cmt-escape"}
+              {:tag (.. tags (special (.-string tags))) :class "cmt-string"}
+              {:tag (.. tags (definition (.-variableName tags))) :class "cmt-variableName"}
+              {:tag (.. tags (local (.-variableName tags))) :class "cmt-variableName"}
+              {:tag (.-typeName tags) :class "cmt-typeName"}
+              {:tag (.-namespace tags) :class "cmt-namespace"}
+              {:tag (.-className tags) :class "cmt-className"}
+              {:tag (.. tags (special (.-variableName tags))) :class "cmt-variableName"}
+              {:tag (.-macroName tags) :class "cmt-macroName"}
+              {:tag (.. tags (definition (.-propertyName tags))) :class "cmt-propertyName"}
+              {:tag (.-comment tags) :class "cmt-comment"}
+              {:tag (.-invalid tags) :class "cmt-invalid"}])))
+
 (def ext #js [cm-clj/default-extensions
-              highlight/classHighlightStyle
+              (syntaxHighlighting highlight-style)
               (.. EditorView -editable (of false))
               cm6-theme])
 
