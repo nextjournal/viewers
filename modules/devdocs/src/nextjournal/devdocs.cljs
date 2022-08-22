c771247152b7bc5ffd3f53247736119984ad0a37
Andrea Amantini <lo.zampino@gmail.com>
2022-08-22 18:04:24 +0200

    cleanup

--- modules/devdocs/src/nextjournal/devdocs.cljs
+++ modules/devdocs/src/nextjournal/devdocs.cljs
@@ -66,42 +66,6 @@
          (map (fn [item] [item-view item]))
          items)])
 
-(comment
-
-  (js/console.log (first js/document.childNodes))
-  (seqable? js/document.childNodes)
-  js/document.childNodes
-
-
-  (->> (.. js/document (querySelectorAll ".font-bold.mt-6"))
-       (map #(.-innerHTML %)))
-
-  (js/console.log (js/Object.keys (first (.. js/document (querySelectorAll ".font-bold.mt-6")))))
-
-  (defmacro for!
-    "Like [[for]], but eager.
-
-    Supports additional key `:into` to coerce into a target collection.
-
-    ```
-    (for! [v [:x :y :z]]
-      v)
-    ;;=> (:x :y :z)
-    ```
-    "
-    {:style/indent 1}
-    [binding & body]
-    (let [pairs (partition 2 binding)
-          coll (some #(when (= :into (first %))
-                        (second %))
-                     pairs)
-          binding (into [] cat (remove #(= :into (first %)) pairs))]
-      (if coll
-        `(into ~coll (for ~binding ~@body))
-        `(doall
-          (for ~binding
-            ~@body))))))
-
 (defn collection-view [collection]
   [:div.overflow-y-auto.bg-white.flex-auto.pb-12.font-sans
    [:div.w-full.max-w-prose.px-8.mx-auto
