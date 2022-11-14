dcca5e2708ccd595b691a46d3c4cfc03b68b380e
Andrea Amantini <lo.zampino@gmail.com>
2022-11-14 17:41:39 +0100

    Remove devdocs skip convention
    this is handled in Ductile now

--- modules/devdocs/src/nextjournal/devdocs.clj
+++ modules/devdocs/src/nextjournal/devdocs.clj
@@ -90,19 +90,12 @@
       (add-collection ["foo/bar/dang" [{:path "foo/bar/dang/z.clj" :title "z"}]])
       (add-collection ["foo/caz" [{:title "w" :path "foo/caz/w.clj"}]])))
 
-(defn excluded? [path]
-  (when (str/ends-with? (str path) ".clj")
-    (-> path fs/file slurp
-        clojure.edn/read-string meta
-        :nextjournal.devdocs/exclude?)))
-
 (defn expand-paths [paths]
   (->> (if (symbol? paths)
          (when-some [ps (some-> paths requiring-resolve deref)]
            (cond-> ps (fn? ps) (apply [])))
          paths)
-       (mapcat (partial fs/glob "."))
-       (remove excluded?)))
+       (mapcat (partial fs/glob "."))))
 
 #_(expand-paths ["docs/**.{clj,md}"
                  "README.md"
